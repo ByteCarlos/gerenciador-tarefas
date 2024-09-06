e004bfe89e4e6cfa8f10d0bb48e2a957bb70df81
Carlos Henrique Santos Silveira <109424074+ByteCarlos@users.noreply.github.com>
2024-09-06 20:36:38 -0300

    fix: datatable implemented from filtered items

--- resources/views/tarefas/index.blade.php
+++ resources/views/tarefas/index.blade.php
@@ -23,9 +23,10 @@
     </main>
     <footer></footer>
     <livewire:scripts />
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js"></script>
     <script src="https://cdn.datatables.net/2.1.5/js/dataTables.min.js"></script>
-    <script src="../js/datatable.js"></script>
+    <script src="{{ asset('js/datatable.js')}}"></script>
 </body>
 </html>
